99bdc88d6b5ae86bb1bda2822b2efab2b19cb1f9
yuiyeong <joyuiyeong@gmail.com>
2024-06-08 16:22:53 +0900

    init

---
+++ dddshop/src/main/kotlin/com/yuiyeong/dddshop/DddshopApplication.kt
@@ -0,0 +1,11 @@
+package com.yuiyeong.dddshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddshopApplication>(*args)
+}
